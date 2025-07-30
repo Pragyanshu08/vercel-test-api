5ba985f849a33493d13ef23e7706c97905833fff
Pragyanshu Tiwari <yashtiwari4000@gmail.com>
2025-07-30 12:43:13 +0530

    3

--- api/index.js
+++ api/index.js
@@ -1,14 +1,7 @@
-const express = require('express');
-const serverless = require('serverless-http');
-
-const app = express();
-
-app.get('/test', (req, res) => {
-  res.json({ message: 'It finally works!' });
-});
-
-app.get('/hello', (req, res) => {
-  res.send('Hello from Vercel Express!');
-});
-
-module.exports = serverless(app);
+module.exports = (req, res) => {
+  const { name = 'World' } = req.query;
+  res.json({ 
+    message: `Hello, ${name}!`,
+    timestamp: new Date().toISOString()
+  });
+};
